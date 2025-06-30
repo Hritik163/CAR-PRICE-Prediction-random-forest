e494657ed2e981c90cfc901fd46853613a560182
Hritik kumar <156803295+Hritik163@users.noreply.github.com>
2025-06-30 18:02:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+app=Flask(__name__)
+## import ridge regresor model and standard scaler pickle
+random_forest_model=pickle.load(open('model/model.pkl','rb'))
+standard_scaler=pickle.load(open('model/scaler.pkl','rb'))
+#Route for home page
+@app.route('/', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        Car_Name = int(request.form.get('Car_Name'))
+        Year = float(request.form.get('Year'))
+        Present_Price = float(request.form.get('Present_Price'))
+        Kms_Driven = float(request.form.get('Kms_Driven'))
+        Fuel_Type = int(request.form.get('Fuel_Type'))
+        Seller_Type = int(request.form.get('Seller_Type'))
+        Transmission = int(request.form.get('Transmission'))
+        Owner = int(request.form.get('Owner'))
+        new_data_scaled = standard_scaler.transform([[Car_Name,Year,Present_Price,Kms_Driven,Fuel_Type,Seller_Type,Transmission,Owner]])
+        result=random_forest_model.predict(new_data_scaled)
+
+        return render_template('home.html', result=result)
+    else:
+        return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
